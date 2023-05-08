f386971f36aeeb7dbcfe46e5ea388fb5271f7762
Terry Galentine Sr <97475279+tgalentinesr-Insight@users.noreply.github.com>
2023-05-08 12:04:05 -0400

    blog

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "aws_network_interface" "foo" {
   }
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
   
