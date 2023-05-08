f1fb183611db1ac99c08d684123c3ded7cc90c48
Terry Galentine Sr <97475279+tgalentinesr-Insight@users.noreply.github.com>
2023-05-08 12:01:12 -0400

    Initial Commit

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ resource "aws_network_interface" "foo" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
+  instance_type = var.instance_type
   
   network_interface {
     network_interface_id = aws_network_interface.foo.id
