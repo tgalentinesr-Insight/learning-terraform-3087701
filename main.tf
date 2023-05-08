f2c305660b82908b77a3338b26fc16e81f1aad81
Terry Galentine Sr <97475279+tgalentinesr-Insight@users.noreply.github.com>
2023-05-08 11:46:46 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,12 +14,39 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_network_interface" "foo" {
+  subnet_id   = aws_subnet.my_subnet.id
+  private_ips = ["10.0.0.50"]
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.nano"
   
   network_interface {
-    network_interface_id = "vpc-0f9c0a50b4874ac2b"
+    network_interface_id = aws_network_interface.foo.id
     device_index         = 0
   }
   tags = {
